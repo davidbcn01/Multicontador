2f640b73e9d475f20ab8f3e94c750ba9c26251a4
David Ferrero <dferre@elpuig.xeill.net>
2020-09-30 18:42:14 +0200

    Initial commit

---
+++ app/src/main/java/com/example/contadordeclics/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.contadordeclics;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    int contador;
+    TextView contadorDeClics;
+    Button augmentarElContador;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        contadorDeClics = findViewById(R.id.;
+        augmentarElContador = findViewById(R.id.);
+
+        augmentarElContador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contador++;
+                contadorDeClics.setText("Has clicado " + contador + " veces");
+            }
+        });
+    }
+}
+
+
+
